f1bd7d47c68d1d8b7441b86ff01fad4c81b4369c
Flynn Davies <fdavies.freelance@gmail.com>
2019-06-05 12:37:22 +0100

    Add in loading animations

--- loading-icons/lib/index.js
+++ loading-icons/lib/index.js
@@ -4,3 +4,15 @@ const squares = document.getElementsByClassName('square');
         this.classList.toggle('loading');
     })
 });
+
+const animationSelect = document.getElementById('animation-type');
+const animationSpeed = document.getElementById('animation-speed');
+const svgObject = document.getElementById('svg-object');
+animationSelect.addEventListener('change', function () {
+    console.log(animationSelect.value);
+    svgObject.style.animationTimingFunction = animationSelect.value;
+});
+animationSpeed.addEventListener('change', function () {
+    console.log(animationSpeed.value);
+    svgObject.style.animationDuration = animationSpeed.value + 's';
+});
